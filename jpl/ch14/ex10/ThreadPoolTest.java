fe91cf625a80ed7d9699c4f4d1484a5929eddef4
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2017-02-27 11:51:32 +0900

    removed imports

--- jpl/ch14/ex10/ThreadPoolTest.java
+++ jpl/ch14/ex10/ThreadPoolTest.java
@@ -10,8 +10,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.junit.Test;
 
